df1f264bcc3e7f2cf5acd1ab09fdd050878156db
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-15 02:49:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     @pigLatinizer = PigLatinizer.new
-    @piglatin = @pigLatinizer.piglatinize("My mom is a warrior")
+    @piglatin = @pigLatinizer.piglatinize("My mom is a prayer and a warrior")
     erb :pigLatinizer
   end
 end
