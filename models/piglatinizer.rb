aa48a984e42c5ac7eb4afd221f2fccf4cca567dc
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-15 02:45:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
   def piglatinize(text)
     text.split(" ").collect do |w|
-      "#{w[/[aeiouy][a-zA-Z]*/]}#{w[/[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]*/]}ay"
+      "#{w[/[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]*/]}".length
     end.join(": ")
   end
 end
